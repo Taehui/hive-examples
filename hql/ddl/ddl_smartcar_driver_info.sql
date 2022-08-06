82696cc1ab99967cdeebdff6a31a7b95240e81bf
Taehui Hong <hongtebari@gmail.com>
2022-08-06 20:24:53 +0900

    Modify driver info schema

--- hql/ddl/ddl_smartcar_driver_info.sql
+++ hql/ddl/ddl_smartcar_driver_info.sql
@@ -1,25 +1,20 @@
 
 CREATE DATABASE IF NOT EXISTS smartcar;
 
-CREATE EXTERNAL TABLE IF NOT EXISTS smartcar.smartcar_status_info (
-	reg_date string,
-	car_number string,
-	tire_fl string,
-	tire_fr string,
-	tire_bl string,
-	tire_br string,
-	light_fl string,
-	light_fr string,
-	light_bl string,
-	light_br string,
-	engine string,
-	break string,
-	battery string
+CREATE EXTERNAL TABLE IF NOT EXISTS smartcar.smartcar_driver_info (
+	r_date string,
+  car_number string,
+  speed_pedal string,
+  break_pedal string,
+  steer_angle string,
+  direct_light string,
+  speed string,
+  area_number string
 )
 PARTITIONED BY( dt string )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 STORED AS textfile
-LOCATION '/user/hive/warehouse/smartcar/smartcar_status_info'
+LOCATION '/user/hive/warehouse/smartcar/smartcar_driver_info'
+TBLPROPERTIES ("skip.header.line.count"="1")
 ;
-
